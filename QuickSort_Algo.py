8d3a5d4b7584c591bf04008c2c51a33c5b2e9f6e
angguotong <36355226+angguotong@users.noreply.github.com>
2021-11-30 01:55:30 +0800

    firstcommit

---
+++ QuickSort_Algo.py
@@ -0,0 +1,31 @@
+#quick sort 
+
+A = [8,16,1]
+
+#Use last number as pivoit
+
+
+def quick_sort (sequence):
+
+    print (sequence)
+
+    lenght = len(sequence)
+
+    if lenght <= 1:
+        return sequence
+    else:
+        pivot = sequence.pop() #This, you will take the last number 
+
+    item_greater = []
+    item_lower = []
+
+    for item in sequence:
+        if item > pivot:
+            item_greater.append(item)
+        else:
+            item_lower.append(item)
+    
+
+    return quick_sort(item_lower) +  [pivot] + quick_sort(item_greater)  
+
+print (quick_sort(A))
