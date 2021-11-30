1a7ca38825da8449e30e2cac381bcfd715bccfe8
angguotong <36355226+angguotong@users.noreply.github.com>
2021-12-01 00:11:10 +0800

    beautify

--- CodilityQ1.py
+++ CodilityQ1.py
@@ -5,19 +5,14 @@ def solution(S):
     compare = ["B", "A" , "L" ,"O", "N"]
 
     Counted_Dct = {
-
     }
-
     for i in compare: 
-
         if i == "L" or i == "O":
             counter = int(S.count(i) /2)
             Counted_Dct[i] = counter 
         else:
             counter = S.count(i)
             Counted_Dct[i] = counter    
-
-    # print (Counted_Dct.values())
 
     answer = min(Counted_Dct.values())
     print (answer)
