7028a5b98811f75bb49b9f2546c9231392ff95ec
angguotong <36355226+angguotong@users.noreply.github.com>
2021-12-02 23:59:35 +0800

    leetcode

---
+++ Easy-PalindromeNumber.py
@@ -0,0 +1,21 @@
+# Input: x = 121
+# Output: true
+
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+# https://leetcode.com/problems/palindrome-number/
+class Solution(object):
+
+    def isPalindrome(self, x):
+        x = list(str(x))
+        middle = len(x) //2 
+        for i, n in enumerate(x[:middle])
+            if x[i] != x[-(i+1)]:
+                return False
+        return True
+
+                
+
+        
+        
