37da956aa7b657d0d6fdcc593abd1ad2fe10d087
angguotong <36355226+angguotong@users.noreply.github.com>
2021-12-05 04:24:40 +0800

    newchanges

--- Easy-LeetCode-TwoSum.py
+++ Easy-LeetCode-TwoSum.py
@@ -1,16 +1,11 @@
-# class Solution(object):
-#     def twoSum(self, nums, target):
-#         for i in range(len(nums)):
-#             Answer = target - nums[i]
-#             if Answer in nums[i+1:]:
-#                 print (i , nums.index(Answer))
-#                 return ([i, nums[i+1:].index(Answer)])
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            Answer = target - nums[i]
+            if Answer in nums[i+1:]:
+                print (i , nums.index(Answer))
+                return ([i, nums[i+1:].index(Answer)])
             
                 
-        
-
-A = [2,7,11,15]
-
-print (A[1:])
-print(A[1:].index(11))           
+              
             
